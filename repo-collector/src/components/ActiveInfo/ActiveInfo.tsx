f1411572bd3a817d0a5828ef660a9f4be882e408
cyc8 <mail@david-mohr.com>
2022-09-21 17:05:59 +0200

    add check if date is today

--- repo-collector/src/components/ActiveInfo/ActiveInfo.tsx
+++ repo-collector/src/components/ActiveInfo/ActiveInfo.tsx
@@ -2,22 +2,37 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 interface ActiveInfoProps {
-  lastCommit: number,
-  published: number,
+  lastCommit: string,
+  published: string,
 }
 
 export const ActiveInfo = ({lastCommit, published}: ActiveInfoProps) => {
+  let publishedFormatted = published.slice(0,10);
+  let lastCommitFormatted = lastCommit.slice(0,10);
+
+  const checkToday = (date: string) => {
+    const today = new Date().toISOString().slice(0,10);
+    if(date === today){
+      return 'today';
+    } else {
+      return date;
+    }
+  }
+
+  lastCommitFormatted = checkToday(lastCommitFormatted);
+  publishedFormatted = checkToday(publishedFormatted);
+
   return(
     <Box sx={{mt: 1, mb: 1}}>
 
       <Typography variant='body1' component='p'>
         <Box component='span' sx={{fontWeight: 500}}>Last Commit: </Box>
-        {lastCommit}
+        {lastCommitFormatted}
       </Typography> 
 
       <Typography variant='body1' component='p'>
         <Box component='span' sx={{fontWeight: 500}}>First Published: </Box>
-        {published}
+        {publishedFormatted}
       </Typography> 
 
     </Box>
