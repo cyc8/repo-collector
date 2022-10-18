14bb9abf06eb6e7902e2b08626c4b06f00f1cfb9
cyc8 <mail@david-mohr.com>
2022-10-18 17:37:12 +0200

    badge count is now attached to every tabId

--- repo-collector/src/App.tsx
+++ repo-collector/src/App.tsx
@@ -14,7 +14,7 @@ const queryClient = new QueryClient();
 export default function App() {
   const [repoUrls, setRepoUrls] = useState<string[]>([]);
 
-  useEffect(() => {   
+  useEffect(() => {
     chrome.tabs && chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
       chrome.tabs.sendMessage(
         // Current tab ID
@@ -24,83 +24,11 @@ export default function App() {
         { type: 'GET_DOM' } as DOMMessage,
 
         // Callback executed when the content script sends a response
-        (response: ReposMessageResponse) => {setRepoUrls(response);});
+        (repoUrls: ReposMessageResponse) => {
+           setRepoUrls(repoUrls);
+      });
     });
-
-    // call handler if url changes
-    chrome.tabs.onUpdated.addListener(handlePageChange);
-
-    // call handler if new tab was activated
-    // chrome.tabs.onActivated.addListener(({ tabId }) => {
-    //   chrome.tabs.get(tabId, handlePageChange);
-    // });
   }, [])
-
-  const handlePageChange = async (tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) => {
-    // prevent accessing chrome specific urls
-    if (tab.url && (tab.url.includes('chrome://') || tab.url.includes('chrome-extension://'))) {
-      return;
-    }
-
-    console.log(tab.status);
-
-
-    chrome.tabs.sendMessage(
-      // Current tab ID
-      tabId || 0,
-      // Message type
-      { type: 'GET_DOM' } as DOMMessage,
-      // Callback executed when the content script sends a response
-      (response: ReposMessageResponse) => {setRepoUrls(response);}
-    );
-
-
-      // ---------- Local Storage - How to ----------
-      // chrome.storage.local.set({processingTabId: tab.id});
-      // ----------
-      // const result = await chrome.storage.local.get(tab.id.toString())
-      //   .then((result) => {return result})
-      //   .catch((rejectReason) => {
-      //     return null;
-      //   });
-
-
-      // if (tab.id.toString() === processingTabId) {
-      //   return;
-      // }
-
-      updateBadge(removeTab, tabId);
-  }
-
-  const updateBadge = (deleteProcessingTabId: Function, tabId: number) => {
-    deleteProcessingTabId(tabId);
-    // chrome.scripting.executeScript({
-    //   target: { tabId: tabId },
-    //   func: () => {
-    //     console.log('executed scripting function');
-    //     console.log(Array.from(document.querySelectorAll('a')));
-    //     chrome.action.setBadgeText({ text: 'red' });
-    //   },
-    // });
-  }
-
-  const removeTab = () => {
-    // TODO
-    // delete tab id property at the end when all done
-    // setProcessingTabId(0);
-  }
-
-  console.log('render');
-  // set badge to number of repos found on page
-  useEffect(() => {
-    // only add badge when in production otherwise it will fail in dev
-    if (process.env.NODE_ENV === 'production') {
-      if(repoUrls){
-        chrome.action.setBadgeText({text: repoUrls.length.toString()});
-        chrome.action.setBadgeBackgroundColor({color: '#ff3737'});
-      }
-    }
-  }, [repoUrls])
 
 
   return (
--- repo-collector/src/chromeServices/serviceWorker.ts
+++ repo-collector/src/chromeServices/serviceWorker.ts
@@ -1,9 +1,38 @@
-const registerEventListener = () => {
-  chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    chrome.action.setBadgeText({ text: 'grr' });
-  });
+import { DOMMessage, ReposMessageResponse } from '../types';
+
+const handlePageChange = async (tabId: number, tab: chrome.tabs.Tab, changeInfo?: chrome.tabs.TabChangeInfo) => {
+  // skip when on browser specific pages
+  if (tab.url && (tab.url.includes('chrome://') || tab.url.includes('chrome-extension://'))) return;
+
+  chrome.tabs.sendMessage(
+    // Current tab ID
+    tabId || 0,
+    // Message type
+    { type: 'GET_DOM' } as DOMMessage,
+    // Callback executed when the content script sends a response
+    (repoUrls: ReposMessageResponse) => {
+      updateBadge(tabId, repoUrls.length);
+    }
+  );
 };
 
-registerEventListener();
+// event listener that fires when ne page has finished loading
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === 'complete') handlePageChange(tabId, tab, changeInfo);
+});
+
+// set badge to number of repos found on page
+const updateBadge = (tabId: number, repoCount: number) => {
+  // return when tabId is not set which is equal 0
+  if (tabId === 0) return;
+
+  // only add badge when in production otherwise it will fail in dev
+  if (process.env.NODE_ENV === 'production') {
+    //display badge when repos were found, otherwise empty string means no badge
+    let badgeCount = repoCount === 0 ? '' : repoCount.toString();
+    chrome.action.setBadgeText({ text: badgeCount, tabId: tabId });
+    chrome.action.setBadgeBackgroundColor({ color: '#ff3737' });
+  }
+};
 
 export {};
