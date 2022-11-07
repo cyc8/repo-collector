2dde39e753f0ac4b42824e4a5e7d697a644c33b4
cyc8 <mail@david-mohr.com>
2022-11-07 14:30:15 +0100

    add popup repocount styling

--- repo-collector/src/Header.tsx
+++ repo-collector/src/Header.tsx
@@ -32,7 +32,16 @@ export default function Headers ({reposCount}:HeaderProps) {
 
       { reposCount !== 0 &&
       <Typography component='p' sx={{color: 'rgba(255,255,255,0.5)', textAlign: 'center', mb: 1.5}}>
-        Found on page: <b>{reposCount}</b>
+        Found on page: 
+          <Box component='span' sx={{
+            color: '#e2e2e2',
+            border: '1px solid',
+            padding: '2px 4px',
+            borderRadius: '5px',
+            ml: 1
+          }}>
+            <b>{reposCount}</b>
+          </Box>
       </Typography> }
     </>
   )
