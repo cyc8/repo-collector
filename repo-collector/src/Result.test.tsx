cb6976aef5bac2e30fca7beb9a787f27f173264a
cyc8 <mail@david-mohr.com>
2022-11-29 10:18:10 +0100

    fix tests

--- repo-collector/src/Result.test.tsx
+++ repo-collector/src/Result.test.tsx
@@ -3,6 +3,6 @@ import {render, screen} from '@testing-library/react';
 
 test("disabled on github", () => {
   render(<Result gitUrls={[]} disabled={true}/>);
-  const disabledInfo = screen.getByText('Disabled on GitHub');
+  const disabledInfo = screen.getByText('Disabled on GitHub, GitLab and Bitbucket');
   expect(disabledInfo).toBeInTheDocument();
 })
